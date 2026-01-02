adb3ae424f44acb9c299f1af7a0ec9e6e894c6a6
김우진 <kedw158@gmail.com>
2026-01-02 09:55:43 +0900

    Styled-Components 과제완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,35 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
+  background-color: black;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+
+    li {
+      list-style: none;
+      font-size: 16px;
+      font-weight: 400;
+    }
+  }
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderContainer>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
+      <ul>
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </HeaderContainer>
   );
 }
